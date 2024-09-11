59367e6d48a5565290c0cea9f2f0f7145583f2e9
Kadircan kahveci <kahvecik19@itu.edu.tr>
2024-09-12 01:37:42 +0300

    modify cartpage to be more usefull

--- PcGearHub/src/Pages/CardPage.jsx
+++ PcGearHub/src/Pages/CardPage.jsx
@@ -1,18 +1,33 @@
-// src/pages/CartPage.jsx
-
-import React, { useContext } from 'react';
-import { Table, Container, Button } from 'react-bootstrap';
-import { AuthContext } from '../contexts/authcontext';
+import React, { useContext, useMemo } from 'react';
+import { Table, Container, Button, Dropdown } from 'react-bootstrap';
 import { ProductContext } from '../contexts/productcontext';
 
 const CartPage = () => {
-  // AuthContext'ten gerekli değerleri al
-  const { cartItems, totalAmount, setCartItems } = useContext(ProductContext);
+  const { cartItems, setCartItems } = useContext(ProductContext);
 
   // Sepeti boşaltma işlevi
   const handleEmptyCart = () => {
     setCartItems([]); // Sepeti boşalt
   };
+
+  // Ürünün miktarını güncelleme işlevi
+  const handleQuantityChange = (productId, quantity) => {
+    setCartItems(prevItems => {
+      // Miktarı sıfır olan ürünü kaldır
+      if (quantity === 0) {
+        return prevItems.filter(item => item.productId !== productId);
+      }
+      // Miktarı güncelle
+      return prevItems.map(item =>
+        item.productId === productId ? { ...item, quantity } : item
+      );
+    });
+  };
+
+  // Toplam tutarı hesaplamak için useMemo kullanımı
+  const totalAmount = useMemo(() => {
+    return cartItems.reduce((total, item) => total + (item.price * (item.quantity || 1)), 0);
+  }, [cartItems]);
 
   return (
     <Container className="mt-4">
@@ -38,11 +53,26 @@ const CartPage = () => {
               {cartItems.map((item, index) => (
                 <tr key={item.productId}>
                   <td>{index + 1}</td>
-                  <td>{item.productName}</td> {/* Ürün ismi */}
+                  <td>{item.name}</td> {/* Ürün ismi */}
                   <td>{item.description}</td> {/* Ürün açıklaması */}
                   <td>${item.price.toFixed(2)}</td> {/* Ürün fiyatı */}
-                  <td>1</td> {/* Varsayılan olarak 1 adet her ürün */}
-                  <td>${item.price.toFixed(2)}</td> {/* Ürün ara toplamı */}
+                  <td>
+                    <Dropdown onSelect={(value) => handleQuantityChange(item.productId, Number(value))}>
+                      <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        {item.quantity || 1}
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu>
+                        {[...Array(10).keys()].map(num => (
+                          <Dropdown.Item key={num + 1} eventKey={num + 1}>
+                            {num + 1}
+                          </Dropdown.Item>
+                        ))}
+                        <Dropdown.Item eventKey={0}>Remove</Dropdown.Item> {/* 0 seçeneği eklenir */}
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </td>
+                  <td>${(item.price * (item.quantity || 1)).toFixed(2)}</td> {/* Ürün ara toplamı */}
                 </tr>
               ))}
             </tbody>
